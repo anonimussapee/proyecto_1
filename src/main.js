92eec7eb377f63f3b191cf2ff0825a5969e1af8c
anonimussapee <librededrogas420@gmail.com>
2022-09-02 23:14:24 -0500

    actualizacion 3 agrege la busqueda por id y or nombre e hize el navbar grid

--- src/main.js
+++ src/main.js
@@ -6,11 +6,29 @@ return valor_bd;
 }
 /*funcion para mostrar la tabla en html*/
 export function show_in_html(donde){
-    let etiquetas=`<th>PRODUCTOS</th><th>CANTIDAD</th><th>COSTO</th><th>ID</th><th>OFERTA</th>`;
+    let etiquetas=`<th>PRODUCTO</th><th>CANTIDAD</th><th>COSTO</th><th>ID</th><th>OFERTA</th>`;
     for(var items of stock){
         etiquetas+=`<tr><td>${items.producto}</td><td>${items.cant}</td><td>${items.costo}</td><td>${items.id}</td><td>${items?.oferta}</td></tr>`;
     }
     donde.innerHTML=etiquetas;
 }
+/*esta función busca los productos por el nombre*/
+export function search_for_name_id(where,word,checki){
+    var data=`<th>PRODUCTO</th><th>CANTIDAD</th><th>COSTO</th><th>ID</th><th>OFERTA</th>`;
+    if(checki.checked){
+        for(var items of stock){
+            if(word==items.id){
+                data+=`<tr><td>${items.producto}</td><td>${items.cant}</td><td>${items.costo}</td><td>${items.id}</td><td>${items.oferta}</td></tr>`
+            }
+        }
+        where.innerHTML=data;
+    }else{
+    for(var items of stock){
+        if(items.producto.includes(word)){
+            data+=`<tr><td>${items.producto}</td><td>${items.cant}</td><td>${items.costo}</td><td>${items.id}</td><td>${items.oferta}</td></tr>`
+        }
+    }
+    where.innerHTML=data;
+}}
 /*esta es la variable donde se guardaran los datos de los productos */
 var stock=[{producto:"plastico transparente 5x6",id:num_stock(),costo:0.60, cant:7, oferta:""},{producto:"plastico transparente 5x6",id:num_stock(),costo:0.60, cant:7, oferta:""},{producto:"plastico transparente 5x6",id:num_stock(),costo:0.60, cant:7, oferta:""},{producto:"plastico transparente 5x6",id:num_stock(),costo:0.60, cant:7, oferta:""},{producto:"plastico transparente 5x6",id:num_stock(),costo:0.60, cant:7, oferta:""},{producto:"plastico transparente 5x6",id:num_stock(),costo:0.60, cant:7, oferta:""},{producto:"plastico transparente 5x6",id:num_stock(),costo:0.60, cant:7, oferta:""},{producto:"plastico transparente 5x6",id:num_stock(),costo:0.60, cant:7, oferta:""},{producto:"plastico transparente 5x6",id:num_stock(),costo:0.60, cant:7, oferta:""},{producto:"plastico transparente 5x6",id:num_stock(),costo:0.60, cant:7, oferta:""},{producto:"plastico transparente 5x6",id:num_stock(),costo:0.60, cant:7, oferta:""},{producto:"plastico transparente 5x6",id:num_stock(),costo:0.60, cant:7, oferta:""},{producto:"plastico transparente 5x6",id:num_stock(),costo:0.60, cant:7, oferta:""},{producto:"plastico transparente 5x6",id:num_stock(),costo:0.60, cant:7, oferta:""},{producto:"plastico transparente 5x6",id:num_stock(),costo:0.60, cant:7, oferta:""},{producto:"plastico transparente 5x6",id:num_stock(),costo:0.60, cant:7, oferta:""},{producto:"plastico transparente 5x6",id:num_stock(),costo:0.60, cant:7, oferta:""},];
--- src/modules.js
+++ src/modules.js
@@ -1,5 +1,17 @@
+/*esta parte del codigo permite desplegar los datos que  hay al cargar el html*/
 var table_products=document.getElementById("tabla_productos")
 window.addEventListener('load', async function(){
 var see= await import("./main.js");
 see.show_in_html(table_products);
-})
+});
+/*esta funcion desplegara los resultados de la busqueda por nombre y id*/
+var table_search_for_name=document.getElementById("tabla_busqueda");
+var btn_search=document.getElementById("btn_buscar");
+
+btn_search.addEventListener('click',async function(){
+    var check_id_ref=document.getElementById("check_id");
+    var word=document.getElementById("buscar_nombre");
+    var word_d=word.value;
+    var search=await import("./main.js");
+    search.search_for_name_id(table_search_for_name,word_d,check_id_ref);
+});
